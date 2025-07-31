3e2af2f0f49e1f73500fe1f5479db0f1c0b275c6
hyewonkim <hyewonds.kim@samsung.com>
2025-07-31 11:44:59 +0900

    [refactor] page 44

--- prime_factors.py
+++ prime_factors.py
@@ -2,14 +2,15 @@ class PrimeFactor:
     def of(self, number):
         factor = []
         if number > 1:
+            divisor = 2
             if number == 4:
-                while number % 2 == 0:
-                    factor.append(2)
-                    number //= 2
+                while number % divisor == 0:
+                    factor.append(divisor)
+                    number //= divisor
             elif number == 6:
-                while number % 2 == 0:
-                    factor.append(2)
-                    number //= 2
+                while number % divisor == 0:
+                    factor.append(divisor)
+                    number //= divisor
                 while number % 3 == 0:
                     factor.append(3)
                     number //= 3
