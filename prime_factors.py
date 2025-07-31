0d3008c5c8125efb12ea09b0af6f02dfeb1cc36a
hyewonkim <hyewonds.kim@samsung.com>
2025-07-31 11:41:18 +0900

    [refactor] page 40

--- prime_factors.py
+++ prime_factors.py
@@ -3,10 +3,7 @@ class PrimeFactor:
         factor = []
         if number > 1:
             if number == 4:
-                if number % 2 == 0:
-                    factor.append(2)
-                    number //= 2
-                if number % 2 == 0:
+                while number % 2 == 0:
                     factor.append(2)
                     number //= 2
             else:
