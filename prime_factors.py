b61cf183f2846955563ecd8e12fbec9adb504006
hyewonkim <hyewonds.kim@samsung.com>
2025-07-31 11:05:47 +0900

    [feature] page 31

--- prime_factors.py
+++ prime_factors.py
@@ -1,4 +1,7 @@
 class PrimeFactor:
     def of(self, param):
         factor = []
+
+        if param == 2:
+            factor.append(2)
         return factor
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -3,3 +3,7 @@ from prime_factors import PrimeFactor
 def test_prime_factor_of_1():
     primefactors = PrimeFactor()
     assert primefactors.of(1) == []
+
+def test_prime_factor_of_2():
+    primefactors = PrimeFactor()
+    assert primefactors.of(2) == [2]
